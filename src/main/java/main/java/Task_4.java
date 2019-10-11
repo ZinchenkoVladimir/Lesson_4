62ada8d4a3d53669d33c494ace0f4a875bd6cc1d
VolodymyrZinchenko <galdrastav@gmail.com>
2019-10-11 15:26:22 +0300

    1

---
+++ src/main/java/main/java/Task_4.java
@@ -0,0 +1,43 @@
+package main.java;
+
+public class Task_4 {
+
+    public static void main(String[] args) {
+    }
+
+    static class Worker {
+
+    }
+
+    static class Clerk extends Worker{
+
+    }
+
+    static class IT extends Worker {
+
+    }
+
+    public static class Programmer extends IT {
+
+    }
+
+    public static class ProjectManager extends IT {
+
+    }
+
+    public static class CTO extends IT {
+
+    }
+
+    public static class OfficeManager extends Clerk {
+
+    }
+
+    public static class HR extends Clerk {
+
+    }
+
+    public static class Cleaner extends Clerk {
+
+    }
+}
